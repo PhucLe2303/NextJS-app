22cd30a32cc3304b4d84510684619f3db16398e0
PhucHCMUS <lehuuphuc23031999@gmail.com>
2021-01-07 11:27:26 +0700

    setup _app, _document, about and index

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useEffect } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+        <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = ()=>{
+
+
+    return(
+        <h1 style={{color:'red'}}>this is about page</h1>
+    );
+}
+
+export default about;
